2105f4faecfcd121c30cdc153fdcf5084c08e13f
Sofiane Beors <theabstractdev@gmail.com>
2016-09-09 12:12:06 +0200

    removed useless folders

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "My_Swift_Meteo_Server",
-    dependencies: [
-    	.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
-    ]
-)
--- build/My_Swift_Meteo.build/Release-iphoneos/My_Swift_Meteo Extension.build/Script-4D892DFC4FC8B79969C4C421.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-My_Swift_Meteo Extension/Pods-My_Swift_Meteo Extension-resources.sh"
-
